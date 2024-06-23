96a940518b800e49265e25af8f177de49b288806
forbiden-west <v5903mt3f@mail.ru>
2024-06-23 16:45:45 +0300

    Upd

--- defaut_lib.py
+++ defaut_lib.py
@@ -1,12 +1,3 @@
-import os
-import time
-import random
-def passwd():
-    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    for i in range(8):
-        passwd0 = ''
-        for i in range(8):
-            passwd0 += random.choice(chars)
-    return passwd0
+import os, random, telebot, time
 def cls():os.system('clear')
 def delay():input()
